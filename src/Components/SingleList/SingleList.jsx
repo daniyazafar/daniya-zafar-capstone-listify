21e75cef0fdb05d50991b3ff8f3a014973f620a1
daniyazafar <daniyazafar@gmail.com>
2024-09-17 13:23:37 -0700

    styled single list page

--- src/Components/SingleList/SingleList.jsx
+++ src/Components/SingleList/SingleList.jsx
@@ -1,12 +1,19 @@
-import { useParams } from "react-router-dom";
-import './SingleList.scss';
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Api } from "../../../utils/utils.js";
+
+import backArrow from "../../../src/assets/icons/back.svg";
+
+import './SingleList.scss';
 
 function SingleList() {
     const api = new Api();
     const params = useParams();
     const [ listDetails, setListDetails ] = useState({});
+
+    const handleBackClick = () => {
+        alert("going back!");
+    }
 
     useEffect(() => {
         const getListDetails = async () => {
@@ -22,7 +29,16 @@ function SingleList() {
 
     return (
         <>
-            <h1>{listDetails.name} - {listDetails.type}</h1>
+        <div className="list">
+            <div className="list__header">
+                <Link to='/home' onClick={handleBackClick}><img src={backArrow} alt="back arrow" /></Link>
+                <h1>{listDetails.name} - {listDetails.type}</h1>
+            </div>
+            <div className="list__area">
+                <div className='list__area-field' contentEditable='true'></div>
+                <div className='list__area-field-tablet' contentEditable='true'></div>
+            </div>
+        </div>
         </>
     )
 }
