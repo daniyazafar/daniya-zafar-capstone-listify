7b7a4b7fbcc598b909395a1aafe67695820bf338
daniyazafar <daniyazafar@gmail.com>
2024-09-19 00:21:19 -0700

    completed single list

--- src/Components/SingleList/SingleList.jsx
+++ src/Components/SingleList/SingleList.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Api } from "../../../utils/utils.js";
 
 import backArrow from "../../../src/assets/icons/back.svg";
@@ -8,39 +8,64 @@ import './SingleList.scss';
 
 function SingleList() {
     const api = new Api();
-    const params = useParams();
-    const [ listDetails, setListDetails ] = useState({});
 
-    const handleBackClick = () => {
-        alert("going back!");
+    const params = useParams();
+    const item_ref = useRef();
+
+    const [ listDetails, setListDetails ] = useState({});
+    const [ items, setListItems ] = useState([]);
+
+    const handleBackClick = async () => {
+        try {
+            const items = item_ref.current.innerText.trim().split('\n');
+    
+            const response = await api.postItems(params.id, items);
+        } catch (error) {
+            console.error("Cannot post item to List: ", error);
+        }
     }
+    
 
     useEffect(() => {
         const getListDetails = async () => {
             try {
                 const response = await api.getListById(params.id);
-                setListDetails(response);
+                if (response) {
+                    const itemsOfList = await api.getListItemsByListId(params.id);
+                    setListDetails(response);
+                    setListItems(itemsOfList);
+                } else {
+                    console.error("List not found");
+                }
             } catch (error) {
                 console.error("Cannot get List: ", error);
             }
         };
         getListDetails();
-    },[])
+    }, [params.id]);
 
+    useEffect(() => {
+        if (item_ref.current) {
+            item_ref.current.innerHTML = items.map(item => `<p>${item.item}</p>`).join('');
+        }
+    }, [items]);
+    
     return (
         <>
-        <div className="list">
-            <div className="list__header">
-                <Link to='/home' onClick={handleBackClick}><img src={backArrow} alt="back arrow" /></Link>
-                <h1>{listDetails.name} - {listDetails.type}</h1>
+            <div className="list">
+                <div className="list__header">
+                    <Link to='/home' onClick={handleBackClick}>
+                        <img src={backArrow} alt="back arrow" />
+                    </Link>
+                    <h1>{listDetails.name} - {listDetails.type}</h1>
+                </div>
+                <div className="list__area">
+                    <div ref={item_ref} className='list__area-field' contentEditable='true'>
+                    </div>
+                </div>
             </div>
-            <div className="list__area">
-                <div className='list__area-field' contentEditable='true'></div>
-                <div className='list__area-field-tablet' contentEditable='true'></div>
-            </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default SingleList
--- utils/utils.js
+++ utils/utils.js
@@ -12,7 +12,7 @@ class Api {
             const response = await axios.get(`${this.url}/lists`);
             return response.data;
         } catch (error) {
-            console.log("Error while fetching all lists from backend: ", error)
+            console.log("Error while fetching all lists from backend: ", error);
         }
     }
 
@@ -21,20 +21,36 @@ class Api {
             const response = await axios.post(`${url}/lists`, newListData);
             return response.data;
         } catch (error) {
-            console.log("Error while posting new list to the backend: ", error)
+            console.log("Error while posting new list to the backend: ", error);
         }
     }
 
     async getListById(id) {
         try {
-            const response = await axios.get(`${this.url}/lists/${id}`)
-            console.log(response.data);
+            const response = await axios.get(`${this.url}/lists/${id}`);
             return response.data;
         } catch (error) {
-            console.error("Error while getting list by id")
+            console.error("Error while getting list by id");
         }
     }
 
+    async postItems(id, items) {
+        try {
+            const response = await axios.post(`${this.url}/lists/${id}/items`, {items});
+            return response.data;
+        } catch (error) {
+            console.error("Could not add items to the list", error);
+        }
+    }
+
+    async getListItemsByListId(id) {
+        try {
+            const response = await axios.get(`${this.url}/lists/${id}/items`);
+            return response.data;
+        } catch (error) {
+            console.error("Could not add items to the database", error);
+        }
+    }
 }
 
 export { Api }
